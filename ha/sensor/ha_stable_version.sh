acc46447b4ff689a90cbd65c2e289afb09726069
Chris Buckley <chris@cmbuckley.co.uk>
2021-12-15 03:46:44 +0000

    command-line sensor for HA stable version

---
+++ ha/sensor/ha_stable_version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+version=$1
+registry=https://registry.hub.docker.com/v2/repositories/homeassistant/$version-homeassistant/tags
+digest=$(curl -s "$registry/stable" | jq -r '.images[0].digest')
+
+value=
+next=$registry
+
+while [ -z "$value" -a -n "$next" ]; do
+    response=$(curl -s "$next")
+    next=$(jq -r '.next' <<< "$response")
+    value=$(jq -r '.results | .[] | select(.images | map(select(.digest == "'$digest'")) | length > 0) | select(.name != "stable")' <<< "$response")
+done
+
+jq -cM --arg d "$digest" 'del(.images) | .digest = $d' <<< "$value"
