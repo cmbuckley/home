c0a6fd653bfc9501cd714f28549462e9789feccb
Chris Buckley <chris@cmbuckley.co.uk>
2021-12-18 21:41:52 +0000

    Heredoc for ha_stable_version jq command

--- ha/sensor/ha_stable_version.sh
+++ ha/sensor/ha_stable_version.sh
@@ -4,13 +4,19 @@ version=$1
 registry=https://registry.hub.docker.com/v2/repositories/homeassistant/$version-homeassistant/tags
 digest=$(curl -s "$registry/stable" | jq -r '.images[0].digest')
 
+read -r -d '' find_tag <<EOS
+.results | .[]
+    | select(.images | map(select(.digest == "$digest")) | length > 0)
+    | select(.name != "stable" and .name != "latest")
+EOS
+
 value=
 next=$registry
 
 while [ -z "$value" -a -n "$next" ]; do
     response=$(curl -s "$next")
     next=$(jq -r '.next' <<< "$response")
-    value=$(jq -r '.results | .[] | select(.images | map(select(.digest == "'$digest'")) | length > 0) | select(.name != "stable" and .name != "latest")' <<< "$response")
+    value=$(jq -r "$find_tag" <<< "$response")
 done
 
 jq -cM --arg d "$digest" 'del(.images) | .digest = $d' <<< "$value"
