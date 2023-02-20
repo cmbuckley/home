a9478eb103efd455b018bd73f5e7b9b52afc82da
Chris Buckley <chris@cmbuckley.co.uk>
2023-02-20 22:42:45 +0000

    Bin schedule improvements
    - Sensor for next bin of any type
    - Notification the day before
    - Download collection dates more frequently
    - Remove today's date from list (and update cron to after collection time)
    - Background task to avoid termination after 60s

--- ha/shell_command/download_bin_schedule.sh
+++ ha/shell_command/download_bin_schedule.sh
@@ -4,8 +4,9 @@
 url="https://opendata.leeds.gov.uk/downloads/bins/dm_jobs.csv"
 output="${0/.sh/.txt}"
 
-curl -s "$url" \
-    | awk -F, -v premise="$1" -v RS=$'\r\n' '$1~premise{print $2, $3}' \
-    | sort -t/ -k3,3 -k2,2 -k1,1 \
-    | sort -u -k1,1 \
-    | sed 's~\(..\)/\(..\)/\(..\)~20\3-\2-\1~' > "$output"
+curl -s "$url" |
+    awk -F, -v p="$1" -v RS=$'\r\n' '$1~p{print $2, $3}' | # rows matching these premises
+    sort -t/ -k3,3 -k2,2 -k1,1 |                           # sort by date
+    grep -v $(date +'%d/%m/%y') |                          # ignore collections today
+    awk '!_[$1]++' |                                       # unique by collection type
+    sed 's~\(..\)/\(..\)/\(..\)~20\3-\2-\1~' > "$output"   # d/m/y -> Y-m-d
