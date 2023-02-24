7c5c8b311764016d3b36fdb2b20981ba15c29234
Chris Buckley <chris@cmbuckley.co.uk>
2023-02-24 14:08:39 +0000

    Use temp file for bin schedule download

--- ha/shell_command/download_bin_schedule.sh
+++ ha/shell_command/download_bin_schedule.sh
@@ -3,10 +3,15 @@
 # https://datamillnorth.org/dataset/household-waste-collections
 url="https://opendata.leeds.gov.uk/downloads/bins/dm_jobs.csv"
 output="${0/.sh/.txt}"
+tmp="$(mktemp)"
 
 curl -s "$url" |
     awk -F, -v p="$1" -v RS=$'\r\n' '$1~p{print $2, $3}' | # rows matching these premises
     sort -t/ -k3,3 -k2,2 -k1,1 |                           # sort by date
     grep -v $(date +'%d/%m/%y') |                          # ignore collections today
     awk '!_[$1]++' |                                       # unique by collection type
-    sed 's~\(..\)/\(..\)/\(..\)~20\3-\2-\1~' > "$output"   # d/m/y -> Y-m-d
+    sed 's~\(..\)/\(..\)/\(..\)~20\3-\2-\1~' > "$tmp"      # d/m/y -> Y-m-d
+
+# atomic replacement of the output file
+# avoids unknown vaules while this script runs
+mv "$tmp" "$output"
