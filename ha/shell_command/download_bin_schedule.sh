ccd44e76d97199e953a93036d66e552d410d36a4
Chris Buckley <chris@cmbuckley.co.uk>
2023-03-05 22:49:45 +0000

    Improve bin schedule/notifications

--- ha/shell_command/download_bin_schedule.sh
+++ ha/shell_command/download_bin_schedule.sh
@@ -6,6 +6,7 @@ output="${0/.sh/.txt}"
 tmp="$(mktemp)"
 
 curl -s "$url" |
+    grep "$1" |                                            # quick filter to speed up the rest
     awk -F, -v p="$1" -v RS=$'\r\n' '$1~p{print $2, $3}' | # rows matching these premises
     sort -t/ -k3,3 -k2,2 -k1,1 |                           # sort by date
     grep -v $(date +'%d/%m/%y') |                          # ignore collections today
