c4bae3bfc51136dc70bcac329b2322ad1df6f9f7
Chris Buckley <chris@cmbuckley.co.uk>
2023-05-19 11:32:40 +0100

    More secure PKCS encryption algorithms
    Plex 1.32 uses OpenSSL 3.0.0, which deprecates older encryption methods.
    However, DSM uses 1.1.1, so need to explicitly provide the encryption
    algorithms, which can't be done using acme.sh.
    
    See https://forums.plex.tv/t/ssl-became-broken-after-latest-pms-update/837416/9

--- scripts/ds/cert/renew.sh
+++ scripts/ds/cert/renew.sh
@@ -8,8 +8,12 @@ $acme/acme.sh --upgrade --home $acme
 $acme/acme.sh --cron --home $acme
 
 if [ $? -eq 0 ]; then
-    $acme/acme.sh --force --toPkcs --home $acme -d $domain --password $password \
-        && mv $acme/$domain/$domain.pfx /etc/ssl/private \
+    openssl pkcs12 -export -out /etc/ssl/private/$domain.pfx \
+        -certpbe AES-256-CBC -keypbe AES-256-CBC -macalg SHA256 \
+        -inkey $acme/$domain/$domain.key \
+        -in $acme/$domain/$domain.cer \
+        -certfile $acme/$domain/ca.cer \
+        -password pass:$password \
         && chmod a+r /etc/ssl/private/$domain.pfx \
         && synopkg restart PlexMediaServer
 fi
