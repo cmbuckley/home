2060ac806bcc9e18ae9d89af1d0951b1f4e97336
Chris Buckley <chris@cmbuckley.co.uk>
2023-09-05 19:01:03 +0100

    update bin script to remove past collections

--- ha/shell_command/download_bin_schedule.sh
+++ ha/shell_command/download_bin_schedule.sh
@@ -4,14 +4,14 @@
 url="https://opendata.leeds.gov.uk/downloads/bins/dm_jobs.csv"
 output="${0/.sh/.txt}"
 tmp="$(mktemp)"
+today="$(date +'%Y-%m-%d')"
 
 curl -s "$url" |
     grep "$1" |                                            # quick filter to speed up the rest
     awk -F, -v p="$1" -v RS=$'\r\n' '$1~p{print $2, $3}' | # rows matching these premises
-    sort -t/ -k3,3 -k2,2 -k1,1 |                           # sort by date
-    grep -v $(date +'%d/%m/%y') |                          # ignore collections today
-    awk '!_[$1]++' |                                       # unique by collection type
-    sed 's~\(..\)/\(..\)/\(..\)~20\3-\2-\1~' > "$tmp"      # d/m/y -> Y-m-d
+    sed 's~\(..\)/\(..\)/\(..\)~20\3-\2-\1~' |             # d/m/y -> Y-m-d
+    sort -k2 |                                             # sort by date
+    awk -v d=$today '$2>d&&!_[$1]++' > "$tmp"              # unique by colour, removing entries before today
 
 # atomic replacement of the output file
 # avoids unknown vaules while this script runs
